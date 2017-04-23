c8d63570a87159120c5709cf0603d2d7a959dd56
Mary Lin <marylin@chicagobooth.edu>
2017-04-23 09:59:54 -0500

    square root form

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -14,11 +14,58 @@ class CalculationsController < ApplicationController
   def flex_payment
     # Parameters: {"num"=>"howdy"}
     int = params["int"].to_f
-    @interest = int/100.to_f
-    @year = params["yr"].to_f
+    @interest = int/100
+    @interest_month = @interest/1200
+    yr = params["yr"].to_f
+    @year = yr*12
     @principle = params["prp"].to_f
-    @monthly = (@intrest*((1+@interest)**@year))/@principle*(((1+@interest)**@year)-1)
+    @month = @principle*((@interest_month)/(1-(1+@interest_month)**-@year))
+    @monthly=@month.round(0)
 
     render("calculations/flex_payment.html.erb")
   end
+  def flex_random
+
+    @min = params[:min].to_f
+    @max = params[:max].to_f
+    @random = rand(@min..@max)
+    render("calculations/flex_random.html.erb")
+  end
+
+
+
+
+  def square_form
+    render("calculations/square_form.html.erb")
+  end
+  def square_results
+    # <!-- Parameters: {"user_number"=>"55"} -->
+    @user_number = params[:user_number].to_f #can use strings or symbols (:user_number) in the params hash
+    @square = @user_number**2
+
+    render("calculations/square_results.html.erb")
+
+  end
+  def square_root_form
+    render("calculations/square_root_form.html.erb")
+  end
+  def square_root_results
+    # <!-- Parameters: {"user_number"=>"55"} -->
+    @user_number = params[:user_number].to_f #can use strings or symbols (:user_number) in the params hash
+    @square_root = Math.sqrt(@user_number)
+
+    render("calculations/square_root_results.html.erb")
+
+  end
+
+
+  # def wine_home
+  #   render("calculations/wine_home.html.erb")
+  # end
+  #
+  # def wine_results
+  #   @wine_search = params[:wine_name]
+  #   render("calculations/wine_results.html.erb")
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   get("/flexible/square/:num",{:controller => "calculations",:action => "flex_square_5"})
   get("/flexible/square_root/:num",{:controller => "calculations",:action => "flex_square_root_5"})
   get("/flexible/payment/:int/:yr/:prp",{:controller => "calculations",:action => "flex_payment"})
+  get("/flexible/random/:min/:max",{:controller => "calculations",:action => "flex_random"})
+
+  get("/square/new",{:controller => "calculations",:action => "square_form"})
+  get("/square/results",{:controller => "calculations",:action => "square_results"})
+  get("/square_root/new",{:controller => "calculations",:action => "square_root_form"})
+  get("/square_root/results",{:controller => "calculations",:action => "square_root_results"})
+
+  # get("/wine/home",{:controller => "calculations",:action => "wine_home"})
+  # get("/wine/results",{:controller => "calculations",:action => "wine_results"})
+
 
 end
